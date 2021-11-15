915582817d501dacbfe290bc24e897288968f644
Yifan Li <mail_liyifan@163.com>
2021-11-15 02:25:10 -0500

    to -> two in run.sh

--- AIMD/QE/run.sh
+++ AIMD/QE/run.sh
@@ -1,4 +1,4 @@
-# use these to commands to run this task
+# use these two commands to run this task
 # add your mpirun commands based on your machine configuration
 cp.x < graphene.gs.in > graphene.gs.out
 cp.x < graphene.in > graphene
